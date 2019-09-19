6069b8dd1c5e3b7033e3f9e9c3e1ba4a2dec6438
Tri Hartanto <dev@trihartanto.com>
2019-09-20 00:47:05 +0700

    Update username

--- example/get-student-bill.php
+++ example/get-student-bill.php
@@ -2,7 +2,7 @@
 require_once dirname(__FILE__) . '/../IDNConnector.php'; // include the main class file
 use IDNConnector\IDNConnector; // since we used namespace you should call the class using "use" or call it directly (new IDNConnector\IDNConnector($user, $pass))
 
-$username = '10014';
+$username = '90001';
 $password = '123';
 $idnConnector = new IDNConnector($username, $password); // Initiate the class and provide valid username and password IDNConnector($username, $password)
 $idnConnector->devMode(); // Use this method to initiated the dev mode. This will use IDN development API URI testclient.infradigital.io
